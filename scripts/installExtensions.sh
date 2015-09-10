c71b2852df3305e96cd3ecb1f1a07fe3bf05b762
dbbaskette <dbbaskette@gmail.com>
2015-09-10 10:41:09 -0400

    Fixed Extension Installs
    pl/perl pl/java

--- scripts/installExtensions.sh
+++ scripts/installExtensions.sh
@@ -8,13 +8,18 @@ install_madlib(){
  source /usr/local/greenplum-db/greenplum_path.sh
  export MASTER_DATA_DIRECTORY=/gpdata/master/gpseg-1
  tar xvfz /tmp/bins/$MADLIB_VERSION.tar --strip=1
+ cd /tmp/bins
  gppkg -i madlib*.gppkg 
  $GPHOME/madlib/bin/madpack install -s madlib -p greenplum -c gpadmin@$SANDBOX.localdomain:5432/gpadmin
  echo "INSTALL PL Extensions"
  gppkg -i $PLR_VERSION.gppkg
+ createlang plr -d gpadmin
  gppkg -i $PLPERL_VERSION.gppkg
+ createlang plperl -d gpadmin
  gppkg -i $PLJAVA_VERSION.gppkg
+ psql -d gpadmin -f $GPHOME/share/postgresql/pljava/install.sql
  gppkg -i $POSTGIS_VERSION.gppkg
+ psql -d gpadmin -f $GPHOME/share/postgresql/contrib/postgis-2.0/postgis.sql
 }
 
 
